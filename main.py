3f32c4f24f59d45cb903070baa3ae0545bf60065
JosepRivaille <josep.de.cid@gmail.com>
2016-02-27 12:27:24 +0100

    Created db schema and python db launcher

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+import sqlite3
+from contextlib import closing
 from flask import Flask, render_template
 app = Flask(__name__)
+
+
+# DB configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app.config.from_object(__name__)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS barcelona (
+	street_code integer PRIMARY KEY NOT NULL,
+	present integer NOT NULL,
+	future integer NOT NULL);
